383616d1f2634fff140c0eefcc06c4ad8a194a4b
vanch <vanch@DESKTOP-585O54I>
2017-03-13 02:13:49 +0700

    admin,layout,detail

---
+++ BookS/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookS.Models;
+
+namespace BookS.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        DataClasses1DataContext _data = new DataClasses1DataContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            var sachL = LaySach(5);
+            
+            return View(sachL);
+        }
+
+        public List<SACH> LaySach(int count)
+        {
+            return _data.SACHes.Take(count).ToList();
+        }
+        
+
+        public ActionResult Chude()
+        {
+            var chude = from cd in _data.CHU_DEs select cd;
+            return PartialView(chude);
+        }
+        public ActionResult NXB()
+        {
+            var chude = from cd in _data.NHA_XUAT_BANs select cd;
+            return PartialView(chude);
+        }
+
+        public ActionResult SPChude(int id)
+        {
+            var sach = from s in _data.SACHes where s.MaCD == id select s;
+            return PartialView(sach);
+        }
+
+        public ActionResult SPNXB(int id)
+        {
+            var sach = from s in _data.SACHes where s.MaNXB == id select s;
+            return PartialView(sach);
+        }
+
+        public ActionResult ChitietSP(int id)
+        {
+            var sach = from s in _data.SACHes where s.MaSach == id select s;
+            return PartialView(sach);
+        }
+
+    }
+}
